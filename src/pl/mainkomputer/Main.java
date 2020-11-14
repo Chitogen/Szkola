89946305e33b3e6e73625e7b32b44a363ab199f5
Chitogen <RusherPGPL@wp.pl>
2020-11-14 15:21:16 +0100

    First Commit

--- src/pl/mainkomputer/Main.java
+++ src/pl/mainkomputer/Main.java
@@ -7,27 +7,24 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("");
-        System.out.println("Podaj nazwe szkoły");
-        System.out.print("Marcin:");
+        System.out.print("Marcin - Podaj nazwe szkoły:");
         String nazwaSzkoły1 = scanner.nextLine();
-        System.out.println("Podaj nazwe szkoły");
-        System.out.print("Zbyszek:");
+        System.out.print("Zbyszek - Podaj nazwe szkoły:");
         String nazwaSzkoły2 = scanner.nextLine();
-        System.out.println("Podaj nazwe szkoły");
-        System.out.print("Mariusz:");
+        System.out.print("Mariusz - Podaj nazwe szkoły:");
         String nazwaSzkoły3 = scanner.nextLine();
 
         System.out.println("");
         Student student1 = new Student("imię:Marcin ",nazwaSzkoły1);
-        System.out.println(student1.imie + "szkoła:"+ nazwaSzkoły1);
+        System.out.println(student1.imie + "szkoła:"+ student1.nazwaSzkoły);
         System.out.println("");
 
         Student student2 = new Student("imię:Zbyszek ",nazwaSzkoły2);
-        System.out.println(student2.imie + "szkoła:"+ nazwaSzkoły2);
+        System.out.println(student2.imie + "szkoła:"+ student2.nazwaSzkoły);
         System.out.println("");
 
         Student student3 = new Student("imię:Mariusz ",nazwaSzkoły3);
-        System.out.println(student3.imie + "szkoła:"+ nazwaSzkoły3);
+        System.out.println(student3.imie + "szkoła:"+ student3.nazwaSzkoły);
         System.out.println("");
     }
 }
